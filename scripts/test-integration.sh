7d23d68617862d34b7bfacf00a514225fe94510b
Zhiheng Yi <zyi093@gmail.com>
2020-08-20 09:49:04 -0400

    fix

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -8,4 +8,4 @@ set -eu
 ARGS=$1
 
 # The idiomatic way to disable test caching explicitly is to use -count=1
-go test -count=1 -race ./test/integration $ARGS
+go test -count=1 -race ./test/integration/... $ARGS
